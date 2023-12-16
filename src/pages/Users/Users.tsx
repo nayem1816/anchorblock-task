a5f96bde095cbf6a35c31d59a1c09a64293d4462
Md Nayem Hossain <nayemhossainmd1@gmail.com>
2023-12-16 22:08:10 +0600

    user page design comlete

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -2,6 +2,8 @@ import { RiDeleteBinLine } from "react-icons/ri";
 import { LuPen } from "react-icons/lu";
 import { useGetAllUsersQuery } from "../../redux/features/users/userSlice";
 import { useState } from "react";
+import { FiPlus } from "react-icons/fi";
+import { LuUploadCloud } from "react-icons/lu";
 
 const Users = () => {
   const [page, setPage] = useState(1);
@@ -23,8 +25,20 @@ const Users = () => {
 
   return (
     <div className="max-w-6xl mx-auto px-4">
-      <h2>Users</h2>
-      <div className="">
+      <div className="flex justify-between mt-10 mb-6 items-center">
+        <h2 className="text-2xl font-semiBold">Users</h2>
+        <div className="buttons flex items-center gap-4">
+          <button className="text-gray-700 border border-gray-700 rounded-lg px-4 py-1 flex justify-center items-center gap-2">
+            <LuUploadCloud />
+            Import
+          </button>
+          <button className="bg-[#6941C6] text-white rounded-lg px-4 py-1 flex justify-center items-center gap-2">
+            <FiPlus />
+            Add User
+          </button>
+        </div>
+      </div>
+      <div className="mb-5">
         <div className="container mx-auto">
           <div className="flex flex-col">
             <div className="overflow-x-auto shadow-md sm:rounded-lg">
